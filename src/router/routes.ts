44673165b2f7b9e14c69f11bb57d90a350320754
0o0 <bbw1996@qq.com>
2023-04-06 18:00:47 +0800

    update

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,7 +7,7 @@ const routes: RouteRecordRaw[] = [
     },
     {
         path: '/home',
-        component: () => import('@/components/Demo')
+        component: () => import('@/view/home/Home.vue')
         
     }
 ]
